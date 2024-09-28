1e01b0babddc49750b976790c2bacb2b22e63865
Eugene Sukhodolin <eugene@sukhodolin.com>
2024-09-28 13:16:24 -0700

    Initial commit (boilerplate)

---
+++ README.md
@@ -0,0 +1,9 @@
+# How to build and run
+
+If you have [Nix](https://nixos.org/) installed with flakes enabled ([The Determinate Nix Installer](https://github.com/DeterminateSystems/nix-installer) enables them by default, for other install options see [NixOS wiki](https://nixos.wiki/wiki/Flakes)) then just run `nix run` in this directory.
+
+If you don't want to bother with Nix, go with the regular C++ routine:
+  * Make sure you have [boost](https://www.boost.org/) installed (with a dev package containing headers)
+  * `cmake -H. -Bbuild/debug`
+  * `cmake --build build/debug`
+  * `./build/debug/logovo/logovo`
---
+++ flake.nix
@@ -0,0 +1,33 @@
+{
+  description = "Log server";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/master";
+    flake-utils.url = "github:numtide/flake-utils";
+    systems.url = "github:nix-systems/default";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, systems, ... }: flake-utils.lib.eachSystem (import systems) (system:
+    let
+      pkgs = import nixpkgs { inherit system; };
+      stdenv = pkgs.stdenv;
+      logovo = stdenv.mkDerivation {
+        name = "logovo";
+        src = ./.;
+        nativeBuildInputs = with pkgs; [ cmake ];
+      };
+    in
+    rec {
+      packages = { inherit logovo; };
+      devShell = with pkgs; mkShell.override { inherit stdenv; } {
+        inputsFrom = [ logovo ];
+        nativeBuildInputs = [ gdb clang-tools_18 ];
+        buildInputs = [ boost ];
+      };
+
+      packages.default = logovo;
+      apps.default.program = "${logovo}/bin/logovo";
+      apps.default.type = "app";
+    }
+  );
+}
